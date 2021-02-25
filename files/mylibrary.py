dc2e54cea729883ebbdd2329977ce93f7535aa71
Mason V. Tea <masonvtea@gmail.com>
2021-02-24 22:37:58 -0500

    Add files via upload

---
+++ files/mylibrary.py
@@ -0,0 +1,13 @@
+'''
+Input: None
+Output: integer describing the meaning of life
+Dependencies: None
+'''
+def meaning_of_life():
+    return 42
+
+def func2():
+    pass
+
+var = meaning_of_life()
+print(var)
